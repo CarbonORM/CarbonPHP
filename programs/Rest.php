99c2558797f904552561b5dd4b45049727fc4bea
rmiles <RichardMiles2@my.unt.edu>
2018-07-02 22:48:22 -0500

    Bad Dir

--- programs/Rest.php
+++ programs/Rest.php
@@ -286,9 +286,7 @@ foreach ($matches as $key => $insert) {// Create Table
 
     $rest['implode'] = ':' . implode(", :", $rest['implode']);
 
-    $verbose and var_dump($rest);
-
-    file_put_contents(__DIR__ . '/../app/Tables/' . $rest['TableName'] . '.php', $mustache($rest));
+    file_put_contents(APP_ROOT . 'table/' . $rest['TableName'] . '.php', $mustache($rest));
 }
 
 
