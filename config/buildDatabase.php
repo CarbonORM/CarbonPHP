f6555552e8ca0d427a04d9c30611837d48add6ac
RichardTMiles <tmiles199@gmail.com>
2019-04-16 15:28:12 -0500

    added column to database

--- config/buildDatabase.php
+++ config/buildDatabase.php
@@ -229,12 +229,15 @@ END;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `carbon_user_messages` (
   `message_id` binary(16) DEFAULT NULL,
+  `from_user_id` binary(16) NOT NULL,
   `to_user_id` binary(16) NOT NULL,
   `message` text NOT NULL,
   `message_read` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`to_user_id`),
   KEY `messages_entity_entity_pk_fk` (`message_id`),
   KEY `messages_entity_user_from_pk_fk` (`to_user_id`),
+  KEY `carbon_user_messages_carbons_entity_pk_fk` (`from_user_id`),
+  CONSTRAINT `carbon_user_messages_carbons_entity_pk_fk` FOREIGN KEY (`from_user_id`) REFERENCES `carbons` (`entity_pk`),
   CONSTRAINT `messages_entity_entity_pk_fk` FOREIGN KEY (`message_id`) REFERENCES `carbons` (`entity_pk`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `messages_entity_user_from_pk_fk` FOREIGN KEY (`to_user_id`) REFERENCES `carbons` (`entity_pk`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
