911b9a1a612d52b740eb8d60760b40bd91a2198c
richardtmiles <richard@miles.systems>
2023-02-18 12:40:12 -0700

    updating awk sed

--- src/extras/replaceInFileSerializeSafe.sh
+++ src/extras/replaceInFileSerializeSafe.sh
@@ -19,7 +19,15 @@ replacement="$5"
 
 cp "$SQL_FILE" "$SQL_FILE.original.sql"
 
-if ! cgrep --quiet "$replace" "$SQL_FILE"; then
+if [ -x /usr/local/bin/cgrep ]
+then
+    GREP=/usr/local/bin/cgrep
+else
+    GREP=/usr/bin/grep
+fi
+
+
+if ! $GREP --quiet "$replace" "$SQL_FILE"; then
 
   echo "${MAGENTA}The string ($replace) was not found in ($SQL_FILE) $NORMAL"
 
@@ -29,13 +37,27 @@ fi
 
 echo "${CYAN}Will replace string ($replace) was found in ($SQL_FILE)${NORMAL}"
 
+if [ -x /usr/local/bin/gawk ]
+then
+    AWK=/usr/local/bin/gawk
+else
+    AWK=/usr/bin/awk
+fi
+
+
+if [ -x /usr/local/bin/gsed ]
+then
+    SED=/usr/local/bin/gsed
+else
+    SED=/usr/bin/sed
+fi
 
 # @link https://stackoverflow.com/questions/29902647/sed-match-replace-url-and-update-serialized-array-count
 # @link https://serverfault.com/questions/1114188/php-serialize-awk-command-speed-up/1114191#1114191
-time ( gsed 's/;s:/;\ns:/g' "$SQL_FILE" |
-  gawk -F'"' '/s:.+'$replaceDelimited'/ {sub("'$replace'", "'$replacement'"); n=length($2)-1; sub(/:[[:digit:]]+:/, ":" n ":")} 1' 2>/dev/null |
-  gsed -e ':a' -e 'N' -e '$!ba' -e 's/;\ns:/;s:/g' |
-  gsed "s/$replaceDelimited/$replacementDelimited/g" > "$SQL_FILE.replaced.sql" )
+time ( $SED 's/;s:/;\ns:/g' "$SQL_FILE" |
+  $AWK -F'"' '/s:.+'$replaceDelimited'/ {sub("'$replace'", "'$replacement'"); n=length($2)-1; sub(/:[[:digit:]]+:/, ":" n ":")} 1' 2>/dev/null |
+  $SED -e ':a' -e 'N' -e '$!ba' -e 's/;\ns:/;s:/g' |
+  $SED "s/$replaceDelimited/$replacementDelimited/g" > "$SQL_FILE.replaced.sql" )
 
 # the pipe above Absolutely MUST to feed into a new file and get moved here below; removing the new file step will cause an empty final file
 mv "$SQL_FILE.replaced.sql" "$SQL_FILE"
